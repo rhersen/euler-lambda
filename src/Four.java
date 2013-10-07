d3d115f3d5a2d2ce7068753e637ddb992ef71184
Rudolf Hersen <rhersen@gmail.com>
2013-10-07 22:30:33 +0200

    four with improved performance

--- src/Four.java
+++ src/Four.java
@@ -1,9 +1,7 @@
 import org.junit.Test;
 
-import java.util.*;
 import java.util.stream.IntStream;
 
-import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,38 +9,45 @@ public class Four {
 
     private int four(int limit) {
         return IntStream.range(0, limit)
+                .parallel()
                 .mapToObj(i -> IntStream.range(0, limit).map(j -> i * j))
                 .flatMapToInt(x -> x)
                 .filter(this::isPalindromic)
-                .max().getAsInt();
+                .max()
+                .getAsInt();
     }
 
     @Test
     public void test() throws Exception {
         assertThat(isPalindromic(9009), is(true));
         assertThat(isPalindromic(9008), is(false));
-        assertThat(digits(9), is(asList(9)));
-        assertThat(digits(11), is(asList(1, 1)));
+        assertThat(reverse(9), is(9));
+        assertThat(reverse(12), is(21));
+        assertThat(reverse(101), is(101));
+        assertThat(reverse(112), is(211));
+        assertThat(reverse(123), is(321));
 
-        System.out.println(four(100));
+        System.out.println(four(1000));
     }
 
-    private List<Integer> digits(int n) {
-        if (n < 10) {
-            return asList(n);
-        } else {
-            int mod = n % 10;
-            List<Integer> r = new ArrayList<>();
-            r.add(mod);
-            r.addAll(digits((n - mod) / 10));
-            return r;
+    private int reverse(int n) {
+        int i = n;
+        int last = i % 10;
+        int result = last;
+
+        while (i >= 10) {
+            i -= last;
+            i /= 10;
+            last = i % 10;
+            result *= 10;
+            result += last;
         }
+
+        return result;
     }
 
     private boolean isPalindromic(int i) {
-        List<Integer> digits = digits(i);
-        LinkedList<Integer> reversed = new LinkedList<>(digits);
-        Collections.reverse(reversed);
-        return digits.equals(reversed);
+        int digits = reverse(i);
+        return digits == i;
     }
 }
